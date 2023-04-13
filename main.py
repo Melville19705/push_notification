ede25b029f6b2710b92fab0854217becc2de79f4
Melville19705 <77281566+Melville19705@users.noreply.github.com>
2023-04-13 19:36:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from pushbullet import Pushbullet
 
+'''
+Don't use this api key it's been reset.
+Get an api key for yourselves at pythonanywhere.com
+Also don't forget to install and sign-in at pushbullet.com on your desktop as well as on your mobile
+to receive push notifications.
+'''
 API_KEY = "o.779Y4vZZGRvL548uSMnOmQtYwsoUS3WK"
 file = "notification.txt"
 
